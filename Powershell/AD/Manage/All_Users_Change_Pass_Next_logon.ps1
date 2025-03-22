7370b1955c484c7643e30a72394b299713432ff0
Josh Ogden <Jogden@diopitt.org>
2025-03-22 17:50:11 -0400

    Update: added variability
    You can now choose x days, define exception group and better output log,

--- Powershell/AD/Manage/All_Users_Change_Pass_Next_logon.ps1
+++ Powershell/AD/Manage/All_Users_Change_Pass_Next_logon.ps1
@@ -3,7 +3,7 @@
 # SUMMARY:      Update Password Change Requirement for Users
 # AUTHOR:       Joshua Ogden
 # DESCRIPTION:  This script updates the "Password change on next logon" flag for 
-#               all users who haven't changed their password in the last 7 days, 
+#               all users who haven't changed their password in the last X days, 
 #               except for those in an exception group, including both service accounts 
 #               and a predefined list of users.
 # COMPATIBILITY: Windows Server 2012 and above
@@ -12,9 +12,9 @@
 #>
 
 # Variable Declaration
-$ExceptionGroup = "CN=ExceptionGroup,OU=Groups,DC=domain,DC=com,"
+$ExceptionGroup = Read-Host "Enter the exception group Distinguished Name (Example: 'CN=ExceptionGroup,OU=Groups,DC=domain,DC=com')" 
 $CurrentDate = Get-Date
-$DaysThreshold = 7
+$DaysThreshold = Read-Host "Enter the number of days to search for users who have not changed their password"
 $DateThreshold = $CurrentDate.AddDays(-$DaysThreshold)
 
 # Import the Active Directory module
@@ -31,13 +31,29 @@ function Get-UserInExceptionGroup {
     return $null -ne $isInExceptionGroup
 }
 
-# Retrieve all users who have not changed their password in the last 7 days
-$Users = Get-ADUser -Filter {PasswordLastSet -lt $DateThreshold} -Properties PasswordLastSet
+# Retrieve all users who have not changed their password in the last X days
+$Users = Get-ADUser -Filter {PasswordLastSet -lt $DateThreshold} -Properties PasswordLastSet | Where-Object { $_.Enabled -eq $true }
 
-foreach ($User in $Users) {
-    if (-not (Get-UserInExceptionGroup -UserDN $User.DistinguishedName)) {
-        try {
-            $User | Set-ADUser -ChangePasswordAtLogon $true
+#write output
+Write-Output "Retrieved users: $($Users.Count)"
+
+# Do you want to output the users?
+$OutputUsers = Read-Host "Enter 'Y' to output the users or 'N' to skip"
+
+if ($OutputUsers -eq 'Y') {
+    # Output the users
+    $Users | Select-Object SamAccountName, Enabled, PasswordLastSet | Export-Csv -Path "C:\temp\All_Users_Change_Pass_Next_logon_$($CurrentDate).csv" -NoTypeInformation
+}
+
+#Do you want to force change password at next logon for these users?
+$UpdateUsers = Read-Host "Enter 'Y' to update the users or 'N' to skip"
+
+if ($UpdateUsers -eq 'Y') {
+    # Process each user
+    foreach ($User in $Users) {
+        if (-not (Get-UserInExceptionGroup -UserDN $User.DistinguishedName)) {
+            try {
+                $User | Set-ADUser -ChangePasswordAtLogon $true
             Write-Output "Updated 'ChangePasswordAtLogon' for user: $($User.SamAccountName)"
         } catch {
             Write-Error "Failed to update 'ChangePasswordAtLogon' for user: $($User.SamAccountName). Error: $_"
@@ -45,4 +61,5 @@ foreach ($User in $Users) {
     } else {
         Write-Output "Skipped user: $($User.SamAccountName) as they are in the exception group."
     }
+}
 }
